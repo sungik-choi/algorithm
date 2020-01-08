323fe7c9f73c0b2b6d35f0089d1f84b57dc93c0b
Sungik Choi <tjddlr92@gmail.com>
2020-01-08 17:14:17 +0900

    탭 간격 수정

--- 2. reverse-integer/solution.js
+++ 2. reverse-integer/solution.js
@@ -2,25 +2,25 @@
  * @param {number} x
  * @return {number}
  */
-const reverse = integer => {
-    let strInteger = String(integer);
-    let reverseInteger = "";
-    let isNegative = false;
-    const negativeSign = "-"
-    const signDigit = strInteger.substr(0, 1);
-    const integerLimit = 2147483648;
+const reverse = (integer) => {
+  let strInteger = String(integer);
+  let reverseInteger = '';
+  let isNegative = false;
+  const negativeSign = '-';
+  const signDigit = strInteger.substr(0, 1);
+  const integerLimit = 2147483648;
 
-    if (signDigit === negativeSign) {
-        const pureNum = strInteger.replace(negativeSign, "");
-        strInteger = pureNum;
-        isNegative = true;
-    }
+  if (signDigit === negativeSign) {
+    const pureNum = strInteger.replace(negativeSign, '');
+    strInteger = pureNum;
+    isNegative = true;
+  }
 
-    for (let i = 0; i < strInteger.length; i += 1) {
-        reverseInteger += strInteger.substr(strInteger.length - i - 1, 1);
-    }
+  for (let i = 0; i < strInteger.length; i += 1) {
+    reverseInteger += strInteger.substr(strInteger.length - i - 1, 1);
+  }
 
-    if (Math.abs(parseInt(reverseInteger, 10)) > integerLimit) return 0;
-    if (isNegative) return parseInt(negativeSign + reverseInteger, 10);
-    return parseInt(reverseInteger, 10);
+  if (Math.abs(parseInt(reverseInteger, 10)) > integerLimit) return 0;
+  if (isNegative) return parseInt(negativeSign + reverseInteger, 10);
+  return parseInt(reverseInteger, 10);
 };
