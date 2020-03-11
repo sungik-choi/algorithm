3cee4ebe40bb7974cbd7d875d04e1cb22092d2bb
최성익 <tjddlr92@gmail.com>
2020-03-11 20:46:00 +0900

    Programmers / 두 정수 사이의 합
    재귀함수로 진행하려고하니 -10,000,000 이상, 10,000,000 이라는 문제 제한조건에서 Maximum Call Stack 에러가 발생한다. 재귀로 해결하기에는 너무나 많은 수라 테스트 케이스가 실패하는 현상 발생. 다른 방식으로 해결하려고 시도중

--- 013. between-sum/solution.js
+++ 013. between-sum/solution.js
@@ -1,25 +1,24 @@
-function solution(a, b) {
-  const callback = (i, arr) => {
-    if (arr[i] + 1 < arr[i + 1]) {
-      arr.splice(i + 1, 0, arr[i] + 1);
-    }
-    console.log(arr);
-    return arr;
-  };
-
-  const arr = [a, b]
-    .sort((a, b) => {
-      return a - b;
-    })
-    .reduce((acc, cur, index, array) => {
-      callback(index, array);
-      if (array[index] + 1 < array[index + 1]) callback(index, array);
-      else {
-        console.log(array[index] + 1, array[index + 1]);
-        return array;
-      }
-    }, 0);
-  return arr;
+function add(arr) {
+  const array = arr;
+  const size = arr.length - 1;
+  const maxNum = arr[size];
+  const targetNum = arr[size - 1];
+  console.log(arr);
+  if (targetNum + 1 >= maxNum) {
+    return array.reduce((acc, cur) => {
+      return acc + cur;
+    });
+  }
+  array.splice(size, 0, targetNum + 1);
+  return add(array);
 }
 
-console.log(solution(9, 3));
+function solution(a, b) {
+  if (a === b) return a;
+  const arr = [a, b].sort((a, b) => {
+    return a - b;
+  });
+  return add(arr);
+}
+
+console.log(solution(1000000, -1000000));
